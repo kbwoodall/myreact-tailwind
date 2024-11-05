b66a7d8c9b0bedf220771148e7ccf75cd9d5e528
Kerry Woodall <kerry.woodall@gmail.com>
2024-11-05 05:00:14 +0000

    chg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Feature from '@components/Feature';
-import Footer from '@components/Footer';
-import logo from '@images/logo.png';
 
 const features = [
   {
@@ -8,19 +6,13 @@ const features = [
     description: 'Run the React app in development mode with live reloading maybe.',
   },
 
-
 ];
 
 const App = () => (
-  <div className='flex min-h-screen flex-col justify-center bg-gray-100 py-6 sm:py-12'>
-    <div className='relative py-3 sm:mx-auto sm:max-w-xl'>
-      <div className='to-light-blue-500 absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-400 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl' />
-      <div className='relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20'>
-        <div className='mx-auto max-w-md'>
-          <div>
-
-          </div>
-          <div className='divide-y divide-gray-200'>
+  <div className='flex-col justify-center bg-gray-300'>
+  
+    
+          
             <div className='space-y-4 py-8 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7'>
               <p>K Woodall  </p>
        
@@ -36,12 +28,20 @@ const App = () => (
 
             </div>
 
-
-          </div>
-        </div>
-      </div>
-    </div>
+          
+   
   </div>
 );
 
 export default App;
+
+/*
+  <div className='relative py-3 sm:mx-auto sm:max-w-xl'>
+      <div className='to-light-blue-500 absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-400 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl' />
+      <div className='relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20'>
+        <div className='mx-auto max-w-md'>
+    
+        </div>
+      </div>
+    </div>
+*/
