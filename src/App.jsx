c3a801d66674ccc913d067d0931b3d39e83f9b23
Kerry Woodall <kerry.woodall@gmail.com>
2024-11-29 16:57:59 +0000

    chg

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,32 @@ const features = [
 
 ];
 
+//<div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8">
+//<div class='flex flex-col items-center h-screen bg-gray-300'>
+
 const App = () => (
-  <div className='flex justify-center justify-items h-screen bg-gray-300'>
-       
-            <div className='mt-10 text-gray-700'>
-              <p>K Woodall  </p>
-       
+
+  <div class='flex flex-col items-center h-screen bg-gray-300'>
+
+    <div class='mt-10 text-red-700 text-sm font-medium'>
+      <p>K Woodall  </p>
+    </div>
+    <div class='mt-10 text-white-700 text-medium font-medium'>
+      <p>K Woodall  </p>
+    </div>
+    <div class='mt-10 text-blue-700 text-lg font-medium'>
+      <p>K Woodall  </p>
+    </div>
+
+  </div>
+);
+
+export default App;
+
+/*
+
+       </div>
+            </div>
               <div className='text-blue-200'>
                 {features.map((feature) => (
                   <Feature
@@ -22,18 +42,9 @@ const App = () => (
                     description={feature.description}
                   />
                 ))}
-              </div>
+              
 
-            </div>
 
-          
-   
-  </div>
-);
-
-export default App;
-
-/*
   <div className='relative py-3 sm:mx-auto sm:max-w-xl'>
       <div className='to-light-blue-500 absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-400 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl' />
       <div className='relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20'>
